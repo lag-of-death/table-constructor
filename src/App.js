90759750cf2e8eea53fac6e464431fad65f8aeaf
Mateusz Wit <mateusz.wit@travelport.com>
2019-03-02 10:42:24 +0100

    Add styled-components

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React  from 'react';
+import styled from 'styled-components';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+const Table = styled.table`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`;
 
-export default App;
+const Row = styled.tr`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`;
+
+const Column = styled.td`
+  padding: 4em;
+  background: papayawhip;
+`;
+
+export default () => (
+  <Table>
+    <tbody>
+      <Row>
+        <Column>
+          Hello World!
+        </Column>
+      </Row>
+    </tbody>
+  </Table>
+);
